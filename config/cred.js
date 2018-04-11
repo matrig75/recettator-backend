956c4f6bea9d2fd95b9d661717b29d89b8c90a37
mattgirard <matrig75@gmail.com>
2018-04-11 11:48:40 +0200

    Correction Ignore Cred v4 (cette fois c'est bon)

--- config/cred.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    credlog : 'userrecettator',
-    credpass : 'passrecettator'
-  };
--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const credlog = require('./cred').credlog;
-const credpass = require('./cred').credpass;
+const credlog = require('./../cred').credlog;
+const credpass = require('./../cred').credpass;
 
 module.exports = {
     url : 'mongodb://'+credlog+':'+credpass+'@ds111885.mlab.com:11885/recettatordb'
